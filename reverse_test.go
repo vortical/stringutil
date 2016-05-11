f3dbb5695d3ba0dd26abe6ed50e55baff125b0a1
Paul Teasdale <paul.teasdale@drillinginfo.com>
2016-05-11 10:48:26 -0400

    initial commit

---
+++ reverse_test.go
@@ -0,0 +1,20 @@
+package stringutil
+
+import "testing"
+
+func TestReverse(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"123", "321"},
+		{"", ""},
+	}
+	for _, c := range cases {
+		got := Reverse(c.in)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+
+}
